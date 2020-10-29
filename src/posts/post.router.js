b6111bc28fbb9bac53e38442d79d4f66932851e6
CalebHolt-15 <calebholt100@gmail.com>
2020-10-29 16:41:44 +0530

    Created Post Router and Controller

---
+++ src/posts/post.router.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.route("/").get(controllers.getAll).post(controllers.createOne);
+
+router
+  .route("/:id")
+  .get(controllers.getOne)
+  .delete(controllers.removeOne)
+  .put(controllers.updateOne);
+
+export default router;
